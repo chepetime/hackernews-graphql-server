e304afa836329b24b66dc857bf1ecb72e96c6ede
José Lugo <chepe.time@gmail.com>
2020-08-15 23:38:47 -0500

    Added pagination, filter and sort

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,27 @@
-function feed(_parent, _args, context, _info) {
-  return context.prisma.link.findMany();
+async function feed(_parent, args, context, _info) {
+  const where =
+    args.filter && args.filter !== ""
+      ? {
+          OR: [
+            { description: { contains: args.filter } },
+            { url: { contains: args.filter } },
+          ],
+        }
+      : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return {
+    links,
+    count,
+  };
 }
 
 module.exports = {
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,16 @@
-const jwt = require("jsonwebtoken");
-
 require("dotenv").config();
 
+const jwt = require("jsonwebtoken");
 const APP_SECRET = process.env.APP_SECRET || "t9nnziUD3syMAQbuWW9n92hkfIR8WV";
 
 function getUserId(context) {
   const Authorization = context.request.get("Authorization");
-
   if (Authorization) {
     const token = Authorization.replace("Bearer ", "");
     const { userId } = jwt.verify(token, APP_SECRET);
     return userId;
   }
+
   throw new Error("Not authenticated");
 }
 
